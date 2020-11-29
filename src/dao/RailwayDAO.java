f25b6a6d026a2643d4a9d3472f09143e968223ff
Carolina <cpprates18@gmail.com>
2020-11-29 12:14:07 -0300

    creating railway dao

--- src/dao/RailwayDAO.java
+++ src/dao/RailwayDAO.java
@@ -4,14 +4,15 @@ import model.Detour;
 import model.Railway;
 import model.Station;
 import model.Terminal;
+import model.Train;
 
 public class RailwayDAO<E> {
     private Terminal<E> a;
     private Terminal<E> b;
-    // private Train[] trains;
+    private Train[] trains;
 
     public RailwayDAO() {
-        // trains = new Train[21];
+        trains = new Train[21];
         buildRailroad();
     }
 
@@ -97,6 +98,208 @@ public class RailwayDAO<E> {
                 ((Detour<E>) newDet).getDetour().setNext(newDet.getPrevious());
             }
         }
+    }
+
+    public String moveTrain(boolean trainA, boolean trainB, int count) {
+        if (count <= 570) {
+            if (trainB || (count % 60 == 0 && count != 0)) {
+                if (canMove(b)) {
+                    b.buildTrain();
+                    trainB = false;
+                    // print train B and infos
+                } else {
+                    trainB = true;
+                }
+            } else if (trainA || (count % 30 == 0 && count != 0)) {
+                if (canMove(a)) {
+                    a.buildTrain();
+                    trainA = false;
+                } else {
+                    trainA = true;
+                }
+            }
+        }
+
+        Railway<E> currA = a;
+        Railway<E> currB = b;
+        System.out.println(this);
+
+        while (currA != null || currB != null) {
+            boolean onHold = false;
+
+            // trens de A => pares, para desviar
+            // verifica caminho de tail (B) para head (A)
+            if (currB.getElement() != null && currB.getElement().getId() % 2 == 0
+                    && currB.getElement().getWait() == 0) {
+                // print trem
+                if (currB instanceof Detour) {
+                    onHold = detourAbound(currB);
+                }
+                // move train
+                if (!onHold && currB.getElement().getWait() == 0) {
+                    // if estacao => embarque e desembarque de passageiros
+                    if (currB instanceof Station) {
+                        ((Station<E>) currB).getExiting();
+                        ((Station<E>) currB).getBoarding();
+                    }
+                    // se nao tem espera, move para proximo pedaco trilho
+                    if (currB.getElement().getWait() == 0) {
+                        currB.getNext().setElement(currB.getElement());
+                        currB.setElement(null);
+                    }
+                }
+            } else if (currB.getElement() != null && currB.getElement().getId() % 2 == 0
+                    && currB.getElement().getWait() != 0) { // reduz tempo
+                // print train
+                currB.getElement().setWait(currB.getElement().getWait() - 1);
+                // move trem
+                if (currB.getElement().getWait() == 0) {
+                    currB.getNext().setElement(currB.getElement());
+                    currB.setElement(null);
+                }
+            }
+
+            onHold = false;
+
+            // trens de B => ímpares, para desviar
+            // verifica caminho de head (A) para tail (B)
+            if (currA.getElement() != null && currA.getElement().getId() % 2 != 0
+                    && currA.getElement().getWait() == 0) {
+                // print train
+                if (currA instanceof Detour)
+                    onHold = detourBbound(currA);
+                // move trem
+                if (!onHold && currA.getElement().getWait() == 0) {
+                    // se estacao
+                    if (currA instanceof Station) {
+                        ((Station<E>) currA).getExiting();
+                        ((Station<E>) currA).getBoarding();
+                    }
+                    // sem tempo de espera
+                    if (currA.getElement().getWait() == 0) {
+                        currA.getPrevious().setElement(currA.getElement());
+                        currA.setElement(null);
+                    }
+                }
+            } else if (currA.getElement() != null && currA.getElement().getId() % 2 != 0
+                    && currA.getElement().getWait() != 0) { // reduz tempo espera
+                // print trem
+                currA.getElement().setWait(currA.getElement().getWait() - 1);
+                // if wait time if over
+                if (currA.getElement().getWait() == 0) {
+                    currA.getPrevious().setElement(currA.getElement());
+                    currA.setElement(null);
+                }
+            }
+
+            // verifica trem no detour
+            if (currA instanceof Detour && ((Detour<E>) currA).getDetour().getElement() != null) {
+                if (((Detour<E>) currA).getDetour().getElement().getWait() > 0) {
+                    ((Detour<E>) currA).getDetour().getElement()
+                            .setWait(((Detour<E>) currA).getDetour().getElement().getWait() - 1);
+                }
+                if (((Detour<E>) currA).getDetour().getElement().getWait() == 0
+                        && ((Detour<E>) currA).getDetour().getNext().getElement() == null
+                        && currA.getNext().getElement() == null && currA.getPrevious().getElement() == null) {
+                    ((Detour<E>) currA).getDetour().getNext().setElement(((Detour<E>) currA).getDetour().getElement());
+                    ((Detour<E>) currA).getDetour().setElement(null);
+                }
+            }
+
+            // move current
+            currA = currA.getNext();
+            currB = currB.getPrevious();
+
+            // salvar trens para relatorio
+            if (a.getElement() != null && a.getElement().getId() % 2 != 0) {
+                trains[a.getElement().getId()] = a.getElement();
+                System.out.println("Train ID#" + a.getElement().getId() + " arrived at terminal A");
+                a.setElement(null);
+            }
+            if (b.getElement() != null && b.getElement().getId() % 2 == 0) {
+                trains[b.getElement().getId()] = b.getElement();
+                System.out.println("Train ID#" + b.getElement().getId() + " arrived at terminal B");
+                b.setElement(null);
+            }
+        }
+        return "" + trainA + " " + trainB;
+    }
+
+    // A => id par | B => id ímpar pq comeca em 0
+    public boolean canMove(Railway<E> current) {
+        if (current.getNext() == null)
+            for (int i = 0; i <= 21; i++) {
+                if (current.getElement() != null && current.getElement().getId() % 2 == 0) // se par (A)
+                    return false;
+                current = current.getPrevious();
+            }
+        else {
+            for (int i = 0; i <= 21; i++) {
+                if (current.getElement() != null && current.getElement().getId() % 2 != 0) // ímpar (B)
+                    return false;
+                current = current.getNext();
+            }
+        }
+        return true;
+    }
+
+    // proximas 20 (A)
+    public boolean isEmptyNext20(Railway<E> current) {
+        Railway<E> curr = current.getNext();
+        for (int i = 0; i <= 21 && curr.getNext() != null; i++) {
+            if (curr.getElement() != null && ((Detour<E>) current).getDetour().getElement() == null) {
+                current.getElement().setWait(i + 2 + curr.getElement().getWait());
+                return false;
+            }
+            curr = curr.getNext();
+        }
+        return true;
+    }
+
+    // detour train A
+    public boolean detourAbound(Railway<E> curr) {
+        if (!isEmptyNext20(curr) && ((Detour<E>) curr).getDetour().getElement() == null) {
+            ((Detour<E>) curr).getDetour().setElement(curr.getElement());
+            System.out.println("Train ID#" + curr.getElement().getId() + " is on hold");
+            curr.setElement(null);
+            return true;
+        }
+        return false;
+    }
+
+    // previous 20 (B)
+    public boolean isEmptyPrevious20(Railway<E> current) {
+        Railway<E> curr = current.getPrevious();
+        for (int i = 0; i <= 21 && curr.getPrevious() != null; i++) {
+            if (curr.getElement() != null && ((Detour<E>) current).getDetour().getElement() == null) {
+                current.getElement().setWait(i + 2 + curr.getElement().getWait());
+                return false;
+            }
+            curr = curr.getPrevious();
+        }
+        return true;
+    }
+
+    // detour train B
+    public boolean detourBbound(Railway<E> curr) {
+        if (!isEmptyPrevious20(curr) && ((Detour<E>) curr).getDetour().getElement() == null) {
+            ((Detour<E>) curr).getDetour().setElement(curr.getElement());
+            System.out.println("Train ID#" + curr.getElement().getId() + " is on hold");
+            curr.setElement(null);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        Railway<E> rail = a;
+        while (rail != null) {
+            ret += rail;
+            rail = rail.getNext();
+        }
+        return ret;
     }
 
 }
--- src/service/TrainService.java
+++
@@ -1,6 +0,0 @@
-package service;
-
-public class TrainService {
-    // faz o gerenciamento da linha férrea
-    
-}
